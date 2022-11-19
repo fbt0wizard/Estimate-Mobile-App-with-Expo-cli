a46cf399ddc8bdb2072f119517bea19c3c1b98fe
fbt0wizard <86288906+fbt0wizard@users.noreply.github.com>
2022-11-19 11:10:20 +0100

    Title fix

--- src/engine/apiCalls.js
+++ src/engine/apiCalls.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { engine } from ".";
 
 export const communicate = async (method, endpoint, body) => {
-  // const url = "http://budgetapp.entkreis.com/api/";
   const url = "http://192.168.8.102:8000/api/";
   const header = await engine.setHeaders();
 
